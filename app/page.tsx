edc6b1ddc59a08eb1824d68d9f6a3221d4ad6aa1
unknown <goelshashank2005@gmail.com>
2026-02-13 07:23:29 +0530

    page layput & styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,48 @@
-import Image from "next/image";
+'use client'
+// to use hooks : useState , useCallback -> which only work in in the client side,
+// Next.js uses server component by default
+
+import { Grid } from '../components/Grid';
+import { useGridState } from '../hooks/useGridState';
 
 export default function Home() {
+  // using custom hook
+   const { grid, onCellClick } = useGridState() ;
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <main className ="min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900 flex flex-col items-center justify-center p-4">
+    {/* Title */}
+    <h1 className="text-5xl font-bold text-white mb-8 text-center">
+      Recursive Grid
+    </h1>
+
+    {/*Instructions */}
+    <div className="text-white text-center mb-6 max-w-2xl">
+      <p className="mb-2"> Click Cells to increment values </p>
+      <p className="text-sm text-gray-300">
+        Divisible by 3 : Right neighbor decrements |
+        Divisible by 5 : Bottom neighbor incremeents |
+        Value ≥ 15 : Lock permanently
+      </p>
     </div>
+
+    {/* Grid */}
+    <Grid grid={grid} onCellClick={onCellClick} />
+
+    {/*Legend */}
+    <div className="mt=8 flex gap-6 text-white">
+      <div className="flex items-center gap-2">
+        <div className="w-6 h-6 bg-blue-500 rounded"></div>
+        <span> Even </span>
+      </div>
+      <div className="felx items-center gap-2">
+        <div className="w-6 h-6 bg-green-500 rounded"></div>
+        <span> Odd</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="w-6 h-6 bg-red-500 rounded"></div>
+        <span> Locked</span>
+      </div>
+    </div>
+    </main>
   );
 }
